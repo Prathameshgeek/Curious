e601869eb17a4fdd238525211fdb2a510c0462fa
Prathamesh <prathamesh@geekyants.com>
2022-09-26 11:24:35 +0530

    New Configuratiom changes

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,17 @@
 const { defineConfig } = require("cypress");
 
-module.exports = defineConfig({
+module.exports = defineConfig({ 
+  
+    requestTimeout: 500000, 
+    numTestsKeptInMemory: 0, // No caching of test cases
+    responseTimeout: 500000,
+    pageLoadTimeout: 500000, 
+    baseUrl: "http://geekyants-login.geekydev.com/home", // base URL for your website
+    chromeWebSecurity: false,
+    watchForFileChanges: true, // Hot reloads the test cases when we make new change
+    viewportHeight: 1259, // height of browser viewport
+    viewportWidth: 1200, // width of browser viewport
+  
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
