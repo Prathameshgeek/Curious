8766ee3246ff3125eedff7756ce841c5893c381c
Prathamesh <prathamesh@geekyants.com>
2022-09-26 12:49:19 +0530

    confi chamges

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,6 @@
 const { defineConfig } = require("cypress");
 
-module.exports = defineConfig({ 
-  
-    requestTimeout: 500000, 
-    numTestsKeptInMemory: 0, // No caching of test cases
-    responseTimeout: 500000,
-    pageLoadTimeout: 500000, 
-    baseUrl: "http://geekyants-login.geekydev.com/home", // base URL for your website
-    chromeWebSecurity: false,
-    watchForFileChanges: true, // Hot reloads the test cases when we make new change
-    viewportHeight: 1259, // height of browser viewport
-    viewportWidth: 1200, // width of browser viewport
-  
+module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
