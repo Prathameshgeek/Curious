5b461e3f5f5acf5c90a7f206b79f07c7b859d34a
Prathamesh <prathamesh@geekyants.com>
2022-10-04 12:43:55 +0530

    xyz on 12:43 4th oct

--- cypress/e2e/HandlingTabs.cy.js
+++ cypress/e2e/HandlingTabs.cy.js
@@ -7,13 +7,12 @@ describe("handling different tabs" ,() => {
 it ("switching between tabs"  , () => {
   cy.visit("https://www.amazon.in/")
   cy.xpath("//a[text()='Mobiles']").click()
-
+  cy.go(-1)
+  cy.go(1)
+  
   cy.xpath("//span[text()='OnePlus Nord CE 2 Lite 5G (Black Dusk, 6GB RAM, 128GB Storage)']").invoke('removeAttr', 'target').click()
-  cy.url().should("eg" , "https://www.amazon.in/OnePlus-Nord-Black-128GB-Storage/dp/B09WQY65HN/ref=lp_1389401031_1_11")
-  cy.go("back")
+  cy.url()
+
+  
 })
-
-
-
-
 })
